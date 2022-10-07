ece368b7c157aa06454b654c925f75a7af30ccd8
Shch <sh82ch@mail.ru>
2022-10-07 15:21:05 +0300

    SELECT

---
+++ INSERT.sql
@@ -0,0 +1,91 @@
+insert into genre(genre_id, name_genre)
+VALUES(1, 'jazz');
+insert into genre(genre_id, name_genre)
+VALUES(2, 'blues');
+insert into genre(genre_id, name_genre)
+VALUES(3, 'pop');
+insert into genre(genre_id, name_genre)
+VALUES(4, 'rap');
+insert into genre(genre_id, name_genre)
+VALUES(5, 'rock');
+
+insert into singer(singer_id, name_singer) 
+values(1, 'Frank Sinatra');
+insert into singer(singer_id, name_singer)
+values(2, 'Louis Armstrong');
+insert into singer(singer_id, name_singer)
+values(3, 'Eric Clapton');
+insert into singer(singer_id, name_singer)
+values (4, 'Katy Perry');
+insert into singer(singer_id, name_singer)
+values(5, 'Lady Gaga');
+insert into singer(singer_id, name_singer)
+values(6, 'Kanye West');
+insert into singer(singer_id, name_singer)
+values(7, 'Snoop Dogg');
+insert into singer(singer_id, name_singer)
+values(8, 'The Beatles');
+
+
+insert into album(id, name_album, release_date)
+values(1, 'The Fame', '01.01.2008');
+insert into album(id, name_album, release_date)
+values(2, 'Pure Gold', '23.12.2005');
+insert into album(id, name_album, release_date)
+values(3, 'Crossroads', '15.07.1988');
+insert into album(id, name_album, release_date)
+values (4, 'Chromatica', '28.05.2020');
+insert into album(id, name_album, release_date)
+values(5, 'Just Dance', '15.07.2008');
+insert into album(id, name_album, release_date)
+values(6,'Graduation', '11.09.2007');
+insert into album(id, name_album, release_date)
+values(7, 'Doggystyle', '18.10.1993');
+insert into album(id, name_album, release_date)
+values(8, 'Love', '06.10.2006');
+
+insert into song(song_id, name_song, song_duration)
+values(1, 'Silent Night', 149);
+insert into song(song_id, name_song, song_duration)
+values(2, 'White Christmas', 200);
+insert into song(song_id, name_song, song_duration)
+values(3, 'Cabaret', 169);
+insert into song(song_id, name_song, song_duration)
+values (4, 'Moon River', 137);
+insert into song(song_id, name_song, song_duration)
+values(5, 'For my love', 148);
+insert into song(song_id, name_song, song_duration)
+values(6, 'Lonely Years', 197);
+insert into song(song_id, name_song, song_duration)
+values(7, 'Hung Up', 336);
+insert into song(song_id, name_song, song_duration)
+values(8, 'Holiday', 368);
+insert into song(song_id, name_song, song_duration)
+values (9, 'Jam', 190);
+insert into song(song_id, name_song, song_duration)
+values(10, 'It’s my life', 260);
+insert into song(song_id, name_song, song_duration)
+values(11, 'Movin On Up', 204);
+insert into song(song_id, name_song, song_duration)
+values (12, 'I get money', 226);
+insert into song(song_id, name_song, song_duration)
+values(13, '99 Problems', 234);
+insert into song(song_id, name_song, song_duration)
+values(14, 'Threat', 245);
+insert into song(song_id, name_song, song_duration)
+values(15, 'Get back', 125);
+insert into song(song_id, name_song, song_duration)
+values(16, 'Because', 164);
+
+
+INSERT INTO
+	collection(collection_id, name_collection, release_date)
+values
+    (1, 'The Best collection', '2022-03-12'),
+    (2, 'Love song collection', '2021-12-20'),
+    (3, 'Star collection', '2018-08-10'),
+    (4, 'Happy collection', '2019-11-15'),
+    (5, 'Meoonlight collection', '2020-07-23'),
+    (6, 'Christmas collection', '2002-06-11'),
+    (7, 'Life collection', '2018-03-10'),
+    (8, 'Celebrity collection', '2020-09-17');
